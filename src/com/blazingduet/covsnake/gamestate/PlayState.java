77dfc9be071a0a232dab1f736581dc2c198f4cdf
ryanf10 <ryanfernaldy10@gmail.com>
2020-12-25 09:47:52 +0700

    minor fix movement bug

--- src/com/blazingduet/covsnake/gamestate/PlayState.java
+++ src/com/blazingduet/covsnake/gamestate/PlayState.java
@@ -12,9 +12,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 
 import com.blazingduet.covsnake.snake.Snake;
 
@@ -29,6 +32,7 @@ public class PlayState extends GameState {
 	private int score;
 	private int movementSpeedDelay, healthDecreaseDelay;
 	private boolean isContinueBannerDrawn;
+	private List<Character> tempMoveInput;
 	
 	public PlayState(JFrame referred) {
 		super(referred);
@@ -38,21 +42,22 @@ public class PlayState extends GameState {
 		continueBanner = loadImg("Continue.png");
 		this.score = 0;
 		this.movementSpeedDelay = 5000;
-		this.healthDecreaseDelay = 100;
+		this.healthDecreaseDelay = 1000;
 		snake = new Snake();
+		tempMoveInput = new ArrayList<>();
 		
 		
 		referred.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if(!snake.isMovingDown() && (e.getKeyChar() == 'w' || e.getKeyChar() == 'W')) {
-					snake.setDirection(true,false,false,false);
-				}else if(!snake.isMovingRight() && (e.getKeyChar() == 'a' || e.getKeyChar() == 'A')) {
-					snake.setDirection(false,false,true,false);
-				}else if(!snake.isMovingUp() && (e.getKeyChar() == 's' || e.getKeyChar() == 'S')) {
-					snake.setDirection(false,true,false,false);
-				}else if(!snake.isMovingLeft() && (e.getKeyChar() == 'd' || e.getKeyChar() == 'D')) {
-					snake.setDirection(false,false,false,true);
+				if(e.getKeyChar() == 'w' || e.getKeyChar() == 'W' || e.getKeyCode() == KeyEvent.VK_UP) {
+					tempMoveInput.add('w');
+				}else if(e.getKeyChar() == 'a' || e.getKeyChar() == 'A' || e.getKeyCode() == KeyEvent.VK_LEFT) {
+					tempMoveInput.add('a');
+				}else if(e.getKeyChar() == 's' || e.getKeyChar() == 'S' || e.getKeyCode() == KeyEvent.VK_DOWN) {
+					tempMoveInput.add('s');
+				}else if(e.getKeyChar() == 'd' || e.getKeyChar() == 'D' || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					tempMoveInput.add('d');
 				}
 			}
 		});
@@ -61,6 +66,7 @@ public class PlayState extends GameState {
 			@Override
 			public void run() {
 				while(snake.getHealthPoint()>0) {
+					checkMove();
 					snake.move();
 					repaint();
 					try {
@@ -106,35 +112,26 @@ public class PlayState extends GameState {
 			return null;
 		}
 	}
-
-	@Override
-	public void render(Graphics g) {
-		g.drawImage(header, 0, 0, null);
-		g.drawString("HP: "+snake.getHealthPoint(),600, 40);
-		
-		g.drawImage(map, 0, 80, null);
-		
-		snake.render(g);
-		
-		if(snake.getHealthPoint() <= 0) {
-			g.drawImage(gameOverBanner, 0, 260, null);
-			if(isContinueBannerDrawn) {
-				g.drawImage(continueBanner, 0, 300, null);
+	
+	private void checkMove() {
+		if(tempMoveInput.size() > 0) {
+			char key = tempMoveInput.get(0);
+			tempMoveInput.remove(0);
+			
+			if(!snake.isMovingDown() && key == 'w') {
+				snake.setDirection(true,false,false,false);
+			}else if(!snake.isMovingRight() && key == 'a') {
+				snake.setDirection(false,false,true,false);
+			}else if(!snake.isMovingUp() && key == 's') {
+				snake.setDirection(false,true,false,false);
+			}else if(!snake.isMovingLeft() && key == 'd') {
+				snake.setDirection(false,false,false,true);
 			}
+			
 		}
-		
-		//map & header border
-		Graphics2D g2 = (Graphics2D) g;
-		Stroke oldStroke = g2.getStroke();
-		g2.setStroke(new BasicStroke(4));
-		Color borderColor = new Color(102,51,0);
-		g2.setColor(borderColor);
-		g2.drawRect(2, 2, 796, 78);
-		g2.drawRect(2, 82, 796, 516);
-		g2.setStroke(oldStroke);
-		
-		
 	}
+
+	
 	
 	private void addListenerGameOver() {
 		referred.removeKeyListener(referred.getKeyListeners()[0]);
@@ -176,6 +173,35 @@ public class PlayState extends GameState {
 			}
 		};
 		continueAnimationThread.start();
+	}
+	
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(header, 0, 0, null);
+		g.drawString("HP: "+snake.getHealthPoint(),600, 40);
+		
+		g.drawImage(map, 0, 80, null);
+		
+		snake.render(g);
+		
+		if(snake.getHealthPoint() <= 0) {
+			g.drawImage(gameOverBanner, 0, 260, null);
+			if(isContinueBannerDrawn) {
+				g.drawImage(continueBanner, 0, 300, null);
+			}
+		}
+		
+		//map & header border
+		Graphics2D g2 = (Graphics2D) g;
+		Stroke oldStroke = g2.getStroke();
+		g2.setStroke(new BasicStroke(4));
+		Color borderColor = new Color(102,51,0);
+		g2.setColor(borderColor);
+		g2.drawRect(2, 2, 796, 78);
+		g2.drawRect(2, 82, 796, 516);
+		g2.setStroke(oldStroke);
+		
+		
 	}
 
 	@Override
