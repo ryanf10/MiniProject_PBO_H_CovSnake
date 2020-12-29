ab27df448440ab6fa1f736f6c57ee20c4e0160f2
GeraldElroy7 <geraldelroy2001@gmail.com>
2020-12-29 12:57:56 +0700

    Adding Hover

--- src/com/blazingduet/covsnake/gamestate/MenuState.java
+++ src/com/blazingduet/covsnake/gamestate/MenuState.java
@@ -14,14 +14,18 @@ public class MenuState extends GameState {
 
 	private final static String DEFAULT_LOCATION = "src/com/blazingduet/covsnake/resources/menu/";
 	
-	private Image backgroundMenu, newGameBtn, highScoreBtn;
+	private Image backgroundMenu, newGameBtn, highScoreBtn, hover_newGameBtn, hover_highScoreBtn;
 
+	boolean isHoverA,isHoverB;
 	
 	public MenuState(JFrame referred) {
 		super(referred);
 		backgroundMenu = this.loadImg("BackgroundMenu.png");
 		newGameBtn = this.loadImg("NewGame.png");
 		highScoreBtn = this.loadImg("HighScore.png");
+		hover_newGameBtn = this.loadImg("NewGameHover.png");
+		hover_highScoreBtn = this.loadImg("HighScoreHover.png");
+		
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -38,6 +42,29 @@ public class MenuState extends GameState {
 			}
 		});
 		
+		addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				
+				//untuk newGameBtn
+				if(e.getPoint().x >=250 && e.getPoint().x <=550 && e.getPoint().y >= 325 && e.getPoint().y <= 405){
+					isHoverA = true;
+					repaint();
+				}else{
+					isHoverA = false;
+					repaint();
+				};
+				
+				//untuk highScoreBtn
+				if(e.getPoint().x >=250 && e.getPoint().x <=550 && e.getPoint().y >= 425 && e.getPoint().y <= 505){
+					isHoverB = true;
+					repaint();
+				}else{
+					isHoverB = false;
+					repaint();
+				};
+			}
+		});
 	}	
 	
 	
@@ -53,8 +80,18 @@ public class MenuState extends GameState {
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(backgroundMenu, 0, 0, null);
-		g.drawImage(newGameBtn, 250, 325, null);
-		g.drawImage(highScoreBtn, 250, 425, null);
+		if(isHoverA) {
+			g.drawImage(hover_newGameBtn, 250, 325, null);
+			g.drawImage(highScoreBtn, 250, 425, null);
+		}
+		else if(isHoverB) {
+			g.drawImage(hover_highScoreBtn, 250, 425, null);
+			g.drawImage(newGameBtn, 250, 325, null);
+		}
+		else {
+			g.drawImage(newGameBtn, 250, 325, null);
+			g.drawImage(highScoreBtn, 250, 425, null);
+		}
 	}
 
 	@Override
